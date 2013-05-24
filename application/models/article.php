c13b7bfd6f19791c61ecb4a64a603bf4fe35e1ee
T <thomasfischer@ok.de>
2013-05-24 09:35:07 +0200

    implemented rateSuggestion for creditpoints

--- application/models/article.php
+++ application/models/article.php
@@ -1,6 +1,7 @@
 <?php
 
 class Article extends Eloquent {
+
     public static $timestamps = true;
     public static $imgSize = 140;
 
@@ -8,23 +9,43 @@ class Article extends Eloquent {
         return 'img/article/';
     }
 
-    public function category()
-    {
+    public function calculateRateAverage() {
+        return DB::table('articles')->where('category_id', '=', $this->category_id)->avg('creditpoints');
+    }
+
+    public function category() {
         return $this->belongs_to('Category');
     }
-    
-    public function condition()
-    {
+
+    public function condition() {
         return $this->belongs_to('Condition');
     }
-    
-    public function maturity()
-    {
+
+    public function maturity() {
         return $this->belongs_to('Maturity');
     }
 
     public function imgPath() {
         return Article::path() . $this->id . '.jpg';
+    }
+
+    public function price() {
+        return $this->price();
+    }
+
+    public function rate() {
+        return $this->rate();
+    }
+
+    public function rateSuggestion() {
+        // calculate current rate
+        $rateCurrent = round(($this->maturity()->factor() + $this->condition()->factor()) * 1, 5 + $this->price() / 10);
+        // get average rate of items of the same category
+        $rateAverage = $this->calculateRateAverage();
+        // get average of rateCurrent and rateAverage
+        $rateSuggested = round(($rateCurrent + $rateAverage) / 2, 0);
+
+        return $rateSuggested;
     }
 
     public function saveImage($image) {
@@ -33,12 +54,8 @@ class Article extends Eloquent {
         }
 
         Resizer::open($image)
-            ->resize(Article::$imgSize , Article::$imgSize , 'crop')
-            ->save(path('public') . $this->imgPath(), 90);
+                ->resize(Article::$imgSize, Article::$imgSize, 'crop')
+                ->save(path('public') . $this->imgPath(), 90);
     }
 
-    public function rateSuggestion() {
-        // here will be some logic
-        return 0;
-    }
 }
