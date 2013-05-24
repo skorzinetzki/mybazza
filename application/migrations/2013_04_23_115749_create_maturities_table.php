c28f1c79a4192ed71f9b9dd35511817ec8fbd88f
T <thomasfischer@ok.de>
2013-05-24 10:03:19 +0200

    change algorithm for rateSuggestion for creditpoints

--- application/migrations/2013_04_23_115749_create_maturities_table.php
+++ application/migrations/2013_04_23_115749_create_maturities_table.php
@@ -17,27 +17,27 @@ class Create_Maturities_Table {
 
         DB::table('maturities')->insert(array(
             'age' => 'less then 3 months',
-            'factor' => 5
+            'factor' => 8
         ));
 
         DB::table('maturities')->insert(array(
             'age' => 'from 3 to 6 months',
-            'factor' => 4
+            'factor' => 6
         ));
 
         DB::table('maturities')->insert(array(
             'age' => 'from 6 months to 1 year',
-            'factor' => 3
+            'factor' => 4
         ));
 
         DB::table('maturities')->insert(array(
             'age' => 'from 1 to 2 years',
-            'factor' => 1
+            'factor' => 2
         ));
 
         DB::table('maturities')->insert(array(
             'age' => 'older than 2 years',
-            'factor' => 0
+            'factor' => 1
         ));
     }
 
