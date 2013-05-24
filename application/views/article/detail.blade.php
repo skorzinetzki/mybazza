a5bcb3c5b341b37fbe230494eb9b8c117b186384
camoser <cm@wahnadium.org>
2013-05-24 21:59:04 +0200

    Minor layout modification

--- application/views/article/detail.blade.php
+++ application/views/article/detail.blade.php
@@ -13,7 +13,7 @@
         <div class="span12"><h4>{{ $article->name }}</h4></div>
     </div>
     <div class="row">
-        <div class="span6 offset1">
+        <div class="span4 offset1">
             <p>{{ $article->description }}</p>
             <dl class="dl-horizontal">
                 <dt>Category</dt>
@@ -22,7 +22,9 @@
                 <dd>{{ $article->price }}</dd>
             </dl>
         </div>
-        <div class="span2"> @include('article.widget') </div>
+        <div class="span2">
+            @include('article.widget')
+        </div>
     </div>
     <div class="row">
         <div class="span6 offset1">
